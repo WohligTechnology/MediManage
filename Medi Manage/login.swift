d2ae4c44ac7e061bd04ef39404fe8a89a1225d7b
TusharSachde <tushar@wohlig.com>
2016-05-04 17:27:30 +0530

    Login Page

---
+++ Medi Manage/login.swift
@@ -0,0 +1,32 @@
+//
+//  login.swift
+//  Medi Manage
+//
+//  Created by Tushar Sachde on 04/05/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import Foundation
+
+class login: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib ()
+        
+    }
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "login", bundle: bundle)
+        let sortnewview = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        sortnewview.frame = bounds
+        sortnewview.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(sortnewview);
+    }
+}
+
