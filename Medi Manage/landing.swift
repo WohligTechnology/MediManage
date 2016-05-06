70f50870897510e3d3b8e0f18ef1f6a6ffa35efd
TusharSachde <tushar@wohlig.com>
2016-05-06 10:51:48 +0530

    3 Pages Done

---
+++ Medi Manage/landing.swift
@@ -0,0 +1,32 @@
+//
+//  landing.swift
+//  Medi Manage
+//
+//  Created by Tushar Sachde on 05/05/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class landing: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib ()
+        
+    }
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "landing", bundle: bundle)
+        let sortnewview = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        sortnewview.frame = bounds
+        sortnewview.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(sortnewview);
+    }
+}
