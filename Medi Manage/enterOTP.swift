b32d2e303ee8f8f207c42d792a3f1b4b15fcdfd4
TusharSachde <tushar@wohlig.com>
2016-05-09 13:36:32 +0530

    qwerty

---
+++ Medi Manage/enterOTP.swift
@@ -0,0 +1,40 @@
+//
+//  enterOTP.swift
+//  Medi Manage
+//
+//  Created by Tushar Sachde on 09/05/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class enterOTP: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib ()
+    }
+    
+    func addPadding(width: CGFloat, myView: UITextField) {
+        let paddingView = UIView(frame: CGRectMake(0, 0, width, myView.frame.height))
+        myView.leftView = paddingView
+        myView.leftViewMode = UITextFieldViewMode.Always
+    }
+    
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "enterOTP", bundle: bundle)
+        let sortnewview = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        sortnewview.frame = bounds
+        sortnewview.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(sortnewview);
+        
+        //        addPadding(15, myView: employeeID)
+    }
+}
---
+++ Medi Manage/retrieveLogin.swift
@@ -0,0 +1,43 @@
+//
+//  retrieveLogin.swift
+//  Medi Manage
+//
+//  Created by Tushar Sachde on 09/05/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class retrieveLogin: UIView {
+    
+    @IBOutlet weak var employeeID: UITextField!
+    @IBOutlet weak var dateOfBirth: UITextField!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib ()
+    }
+    
+    func addPadding(width: CGFloat, myView: UITextField) {
+        let paddingView = UIView(frame: CGRectMake(0, 0, width, myView.frame.height))
+        myView.leftView = paddingView
+        myView.leftViewMode = UITextFieldViewMode.Always
+    }
+    
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "retrieveLogin", bundle: bundle)
+        let sortnewview = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        sortnewview.frame = bounds
+        sortnewview.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(sortnewview);
+        
+        addPadding(15, myView: employeeID)
+        addPadding(15, myView: dateOfBirth)
+    }
+}
