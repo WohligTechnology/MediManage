b1e6a91210a05dc6c1d7025797b5954ffe9db791
Harsh Thakkar <harshthakkar46@gmail.com>
2016-05-13 15:31:06 +0530

    member list border added

--- Medi Manage/memberList.swift
+++ Medi Manage/memberList.swift
@@ -13,6 +13,14 @@ import UIKit
     @IBOutlet weak var dummyButton: UIButton!
     @IBOutlet var memberListMainView: UIView!
     
+    @IBOutlet weak var nameViewOne: UIView!
+    @IBOutlet weak var dobViewOne: UIView!
+    @IBOutlet weak var domViewOne: UIView!
+    
+    @IBOutlet weak var nameViewTwo: UIView!
+    @IBOutlet weak var domViewTwo: UIView!
+    @IBOutlet weak var dobViewTwo: UIView!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         loadViewFromNib ()
@@ -21,6 +29,13 @@ import UIKit
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         loadViewFromNib ()
+    }
+    
+    func addBottomBorder(color: UIColor, width: CGFloat, myView: UIView) {
+        let border = CALayer()
+        border.backgroundColor = color.CGColor
+        border.frame = CGRectMake(5, myView.frame.size.height, myView.frame.size.width - 10, width)
+        myView.layer.addSublayer(border)
     }
     
     func loadViewFromNib() {
@@ -43,9 +58,17 @@ import UIKit
         let mainheader = header(frame: CGRectMake(0, 20, width, 70))
         self.addSubview(mainheader)
         
-        memberListMainView.frame = CGRectMake(0, 90, self.frame.size.width, self.frame.size.height);
+        memberListMainView.frame = CGRectMake(0, 90, self.frame.size.width, self.frame.size.height)
         
         dummyButton.layer.zPosition = 10000
+        
+        //add borders
+        addBottomBorder(UIColor.blackColor(), width: 0.5, myView: nameViewOne)
+        addBottomBorder(UIColor.blackColor(), width: 0.5, myView: dobViewOne)
+        addBottomBorder(UIColor.blackColor(), width: 0.5, myView: domViewOne)
+        addBottomBorder(UIColor.blackColor(), width: 0.5, myView: nameViewTwo)
+        addBottomBorder(UIColor.blackColor(), width: 0.5, myView: dobViewTwo)
+        addBottomBorder(UIColor.blackColor(), width: 0.5, myView: domViewTwo)
     }
 
     @IBAction func premiumcalculationCall(sender: AnyObject) {
