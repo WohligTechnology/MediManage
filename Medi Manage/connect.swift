13dbb1d72fe5953d22be5d85f9436ed97ff9e79d
Harsh Thakkar <harshthakkar46@gmail.com>
2016-05-14 18:52:36 +0530

    connect kar de

---
+++ Medi Manage/connect.swift
@@ -0,0 +1,40 @@
+//
+//  connect.swift
+//  Medi Manage
+//
+//  Created by Harsh Thakkar on 14/05/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import UIKit
+
+class connect: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib ()
+    }
+    
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "connect", bundle: bundle)
+        let connect = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        connect.frame = bounds
+        connect.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(connect)
+    }
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
