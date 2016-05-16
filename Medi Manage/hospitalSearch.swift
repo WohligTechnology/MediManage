3567028352a33612ae423ac9e58c588900074814
Harsh Thakkar <harshthakkar46@gmail.com>
2016-05-16 13:31:15 +0530

    :hospital: mein operation chalu hai :joy:

--- Medi Manage/hospitalSearch.swift
+++ Medi Manage/hospitalSearch.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-class hospitalSearch: UIView {
+@IBDesignable class hospitalSearch: UIView {
+    
+    @IBOutlet var hospitalSearchMainView: UIView!
+    @IBOutlet weak var yourLocation: UITextField!
+    @IBOutlet weak var hospitalName: UITextField!
+    @IBOutlet weak var greenIcon: UIImageView!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -20,6 +25,14 @@ class hospitalSearch: UIView {
         loadViewFromNib ()
     }
     
+    func addBottomBorder(color: UIColor, linewidth: CGFloat, myView: UIView) {
+        let border = CALayer()
+        border.backgroundColor = color.CGColor
+        //border.frame = CGRectMake(0, myView.frame.size.height - width, myView.frame.size.width, width)
+        border.frame = CGRectMake(0, myView.frame.size.height - linewidth, width - 30, linewidth)
+        myView.layer.addSublayer(border)
+    }
+    
     func loadViewFromNib() {
         let bundle = NSBundle(forClass: self.dynamicType)
         let nib = UINib(nibName: "hospitalSearch", bundle: bundle)
@@ -27,6 +40,31 @@ class hospitalSearch: UIView {
         hospitalSearch.frame = bounds
         hospitalSearch.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
         self.addSubview(hospitalSearch)
+        
+        let statusBar = UIView(frame: CGRectMake(0, 0, width, 20))
+        statusBar.backgroundColor = UIColor(red: 62/255, green: 62/255, blue: 62/255, alpha: 1)
+        self.addSubview(statusBar)
+        
+        let mainheader = header(frame: CGRectMake(0, 20, width, 50))
+        self.addSubview(mainheader)
+        
+        let mainsubHeader = subHeader(frame: CGRectMake(0, 70, width, 50))
+        mainsubHeader.subHeaderIcon.image = UIImage(named: "footer_five")
+        mainsubHeader.subHeaderTitle.text = "HOSPITAL SEARCH"
+        self.addSubview(mainsubHeader)
+        
+        let mainfooter = footer(frame: CGRectMake(0, height - 55, width, 55))
+        mainfooter.layer.zPosition = 1000
+        self.addSubview(mainfooter)
+        
+        hospitalSearchMainView.frame = CGRectMake(0, 120, self.frame.size.width, self.frame.size.height - 55)
+        
+        greenIcon.layer.cornerRadius = 2
+        greenIcon.clipsToBounds = true
+        
+        // add borders
+        addBottomBorder(UIColor.blackColor(), linewidth: 0.5, myView: yourLocation)
+        addBottomBorder(UIColor.blackColor(), linewidth: 0.5, myView: hospitalName)
     }
 
     /*
