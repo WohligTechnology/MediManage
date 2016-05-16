f8176e8ea95183d55d93b57e4d6e4a0f669dee37
Harsh Thakkar <harshthakkar46@gmail.com>
2016-05-16 16:27:18 +0530

    BHAGO!! :house: mein aag lagi :fire: :fire_engine:

---
+++ Medi Manage/homeMenus.swift
@@ -0,0 +1,40 @@
+//
+//  homeMenus.swift
+//  Medi Manage
+//
+//  Created by Harsh Thakkar on 16/05/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import UIKit
+
+class homeMenus: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib ()
+    }
+    
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "homeMenus", bundle: bundle)
+        let homeMenus = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        homeMenus.frame = bounds
+        homeMenus.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(homeMenus)
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
