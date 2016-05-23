d7b7a8d041bf0d4d5a3134945122e5654f51ff2e
Harsh Thakkar <harshthakkar46@gmail.com>
2016-05-23 19:11:20 +0530

    splah screen is not on my sid side

--- Medi Manage/SplashScreenController.swift
+++ Medi Manage/SplashScreenController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class SplashScreenController: UIViewController {
     
-    @IBOutlet var splashScreenMainView: UIView!
-    
     var backgroundImage: String!
     var pageIndex: Int!
 
@@ -23,6 +21,7 @@ class SplashScreenController: UIViewController {
         let myimage = UIImageView(frame: CGRectMake(0, 0, width, height))
         myimage.image = image
         self.view.addSubview(myimage)
+        
         
         // Do any additional setup after loading the view.
     }
