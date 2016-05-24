3e2106385c46e1fbe23a6da67d3e705ac9963459
Harsh Thakkar <harshthakkar46@gmail.com>
2016-05-24 11:49:44 +0530

    back to normal

---
+++ Medi Manage/TutorialController.swift
@@ -0,0 +1,39 @@
+//
+//  TutorialController.swift
+//  MediManage
+//
+//  Created by Harsh Thakkar on 24/05/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import UIKit
+
+var gTutorialController: UIViewController!
+
+class TutorialController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        gTutorialController = self
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Medi Manage/tutorial.swift
@@ -0,0 +1,40 @@
+//
+//  tutorial.swift
+//  MediManage
+//
+//  Created by Harsh Thakkar on 24/05/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import UIKit
+
+class tutorial: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib ()
+    }
+    
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "tutorial", bundle: bundle)
+        let tutorial = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        tutorial.frame = bounds
+        tutorial.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(tutorial)
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
