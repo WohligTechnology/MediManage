17eccb475e0f3c9760c9a73b2f4933e72fe74dbd
Harsh Thakkar <harshthakkar46@gmail.com>
2016-07-02 11:50:02 +0530

    accordion

--- Medi Manage/InsuredMembersController.swift
+++ Medi Manage/InsuredMembersController.swift
@@ -10,12 +10,27 @@ import UIKit
 
 var gInsuredMembersController: UIViewController!
 
-class InsuredMembersController: UIViewController {
+class InsuredMembersController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let relations = ["Self", "Mother", "Father"]
+    var selectedIndexPath: NSIndexPath?
+    @IBOutlet var insuredMemberMainView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         gInsuredMembersController = self
         // Do any additional setup after loading the view.
+        
+        let statusBar = UIView(frame: CGRectMake(0, 0, width, 20))
+        statusBar.backgroundColor = UIColor(red: 62/255, green: 62/255, blue: 62/255, alpha: 1)
+        self.view.addSubview(statusBar)
+        
+        let mainheader = header(frame: CGRectMake(0, 20, width, 50))
+        self.view.addSubview(mainheader)
+        
+        let mainfooter = footer(frame: CGRectMake(0, height - 55, width, 55))
+        mainfooter.layer.zPosition = 1000
+        self.view.addSubview(mainfooter)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +38,61 @@ class InsuredMembersController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return relations.count
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let previousIndexPath = selectedIndexPath
+        if indexPath == selectedIndexPath {
+            selectedIndexPath = nil
+        } else {
+            selectedIndexPath = indexPath
+        }
+        var indexPaths : Array<NSIndexPath> = []
+        if let previous = previousIndexPath {
+            indexPaths += [previous]
+        }
+        if let current = selectedIndexPath {
+            indexPaths += [current]
+        }
+        if indexPaths.count > 0 {
+            tableView.reloadRowsAtIndexPaths(indexPaths, withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
+    }
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        (cell as! InsuredMemberCell).watchFrameChanges()
+    }
+    
+    func tableView(tableView: UITableView, didEndDisplayingCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        (cell as! InsuredMemberCell).ignoreFrameChanges()
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+//        if indexPath.row == 0 {
+//            return InsuredMemberCell.expandedHeight
+//        }
+        if indexPath == selectedIndexPath {
+            return InsuredMemberCell.expandedHeight
+        } else {
+            return InsuredMemberCell.defaultHeight
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("insuredcell", forIndexPath: indexPath) as! InsuredMemberCell
+        
+        cell.selectionStyle = .None
+        
+        cell.relationTitle.text = relations[indexPath.item]
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
@@ -34,4 +104,45 @@ class InsuredMembersController: UIViewController {
     }
     */
 
+}
+
+class InsuredMemberCell: UITableViewCell {
+    
+    @IBOutlet weak var firstNameView: UIView!
+    @IBOutlet weak var middleNameView: UIView!
+    @IBOutlet weak var lastNameView: UIView!
+    @IBOutlet weak var dobView: UIView!
+    @IBOutlet weak var sumView: UIView!
+    @IBOutlet weak var topupSumView: UIView!
+    @IBOutlet weak var balanceView: UIView!
+    
+    @IBOutlet weak var relationTitle: UILabel!
+    
+    class var expandedHeight: CGFloat { get { return 350 } }
+    class var defaultHeight: CGFloat { get { return 40 } }
+    
+    func checkHeight() {
+        firstNameView.hidden = (frame.size.height < InsuredMemberCell.expandedHeight)
+        middleNameView.hidden = (frame.size.height < InsuredMemberCell.expandedHeight)
+        lastNameView.hidden = (frame.size.height < InsuredMemberCell.expandedHeight)
+        dobView.hidden = (frame.size.height < InsuredMemberCell.expandedHeight)
+        sumView.hidden = (frame.size.height < InsuredMemberCell.expandedHeight)
+        topupSumView.hidden = (frame.size.height < InsuredMemberCell.expandedHeight)
+        balanceView.hidden = (frame.size.height < InsuredMemberCell.expandedHeight)
+    }
+    
+    func watchFrameChanges() {
+        addObserver(self, forKeyPath: "frame", options: .New, context: nil)
+        checkHeight()
+    }
+    
+    func ignoreFrameChanges() {
+        //removeObserver(self, forKeyPath: "frame")
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if keyPath == "frame" {
+            checkHeight()
+        }
+    }
 }
