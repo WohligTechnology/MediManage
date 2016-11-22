d7eed068101deefced0cd1d640c53741a723b839
Pranayj14 <pranayj1449517@gmail.com>
2016-11-22 12:49:53 +0530

    New demo

--- Medi Manage/newDemoViewController.swift
+++ Medi Manage/newDemoViewController.swift
@@ -1,87 +1,95 @@
 //
-//  ReimbursementOneController.swift
+//  newDemoViewController.swift
 //  MediManage
 //
-//  Created by Pranay Joshi on 12/11/16.
+//  Created by Pranay Joshi on 22/11/16.
 //  Copyright © 2016 Wohlig Technology. All rights reserved.
 //
 
 import UIKit
 import SwiftyJSON
-class ReimbursementOneController: UIViewController,UIScrollViewDelegate {
-    @IBOutlet weak var knowMorePressed: UIButton!
+
+class newDemoViewController: UIViewController {
+
+    var amountReimburseTwo:String = ""
+    var AdmissionTwoString = ""
     var claimsJSON: JSON!
     var closedClaimsJSON: JSON!
     var pendingClaimJSON: JSON!
-    var personJson : JSON! = []
-    var status1:String! = ""
-    var status2:String! = ""
-    var status3:String! = ""
-    var status4:String! = ""
-    var mainStatusLabelDate:String! = ""
-    var mainStatusLabel:String! = ""
-    @IBOutlet weak var claimNo: UILabel!
-    var name:String! = ""
-    var id:String! = ""
-    var amount:String! = ""
-    @IBOutlet weak var settledDate: UILabel!
-    @IBOutlet weak var fourthStatus: UILabel!
-   
-    @IBOutlet weak var mainStatusDate: UILabel!
-        @IBOutlet weak var thirdStatus: UILabel!
-    @IBOutlet weak var secondStatus: UILabel!
-    @IBOutlet weak var firstStatus: UILabel!
+    var nameReimburseTwo: String = ""
+    @IBOutlet weak var nameTwo: UILabel!
+    @IBOutlet weak var claimSupport: UILabel!
     
-    @IBOutlet weak var mainStatus: UILabel!
-    @IBOutlet weak var amountLabel: UILabel!
-    @IBOutlet weak var idLabel: UILabel!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var patientNameReimburse: UILabel!
-    @IBOutlet weak var reimbursementOneScroll: UIScrollView!
+    @IBOutlet weak var admissionTwo: UILabel!
+    @IBOutlet weak var amountTwo: UILabel!
+    @IBOutlet weak var noTwo: UILabel!
+    @IBOutlet weak var popupView: UIStackView!
+    @IBOutlet weak var number1: UIButton!
+    @IBOutlet weak var patientNameReimburseTwo: UILabel!
     
-    @IBOutlet weak var pendingStatus: UIImageView!
-    @IBOutlet weak var queryStatus: UIImageView!
+    @IBOutlet weak var claimNoReimburseTwo: UILabel!
     
-    @IBOutlet weak var knowMore: UIButton!
-    @IBOutlet weak var RequestRecdeivedStatus: UIImageView!
-    @IBOutlet weak var requestedAmountReimburseOne: UILabel!
+    @IBOutlet weak var dateofAdmissionReimburseTwo: UILabel!
+    
+    @IBOutlet weak var requestReceivedImage: UIImageView!
+    @IBOutlet weak var requestedAmountReimburseTwo: UILabel!
+    
+    @IBOutlet weak var pendingImage: UIImageView!
+    
+    @IBOutlet weak var settledImage: UIImageView!
+    @IBOutlet weak var reimburseTwoScroll: UIScrollView!
+    
+    @IBOutlet weak var number2: UIButton!
+    @IBOutlet weak var reimburseView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        reimbursementOneScroll.contentSize.height = 667
-        reimbursementOneScroll.contentSize.width = 375
-        reimbursementOneScroll.minimumZoomScale = 0.03
-        reimbursementOneScroll.maximumZoomScale = 1.0
+        reimburseTwoScroll.contentSize.height = 667
+        reimburseTwoScroll.contentSize.width = 375
+        reimburseTwoScroll.minimumZoomScale = 0.03
+        reimburseTwoScroll.maximumZoomScale = 1.0
+        self.popupView.alpha = 0
+        self.reimburseView.alpha = 1
+        popupView.layer.masksToBounds = true
+        self.popupView.alpha = 0
+        
         navshow()
-        print("personJson ")
-        print(personJson)
-//      mainStatusLabelDate = personJson["PendingClaims"][0]["ClaimsStatus"][0]["ClaimDate"].stringValue.characters.split{$0 == "T"}.map(String.init)
-        nameLabel.text = name
-        idLabel.text = id
-        amountLabel.text = amount
-        firstStatus.text = status1
-        secondStatus.text = status2
-        thirdStatus.text = status3
-        fourthStatus.text = status4
-        mainStatus.text = mainStatusLabel
-//        var a = mainStatusLabelDate!.characters.split{$0 == "T"}.map(String.init)
-      //  print(mainStatusLabelDate)
-//        mainStatusDate.text = a[0]
-        let mainSubHeader = subHeader(frame: CGRectMake(0, 60, width, 50))
-        mainSubHeader.subHeaderTitle.text = "REIMURSEMENT CLAIM"
-        mainSubHeader.subHeaderIcon.image = UIImage(named: "my_claim_icon")
-        self.view.addSubview(mainSubHeader)
-       // patientNameReimburse.text = name
-        func addBottomBorder(color: UIColor, linewidth: CGFloat, myView: UIView) {
+                nameTwo.text = nameReimburseTwo
+                
+        func addBottomBorder(color: UIColor, linewidth: CGFloat, myView:UIView) {
             let border = CALayer()
             border.backgroundColor = color.CGColor
-            //border.frame = CGRectMake(0, myView.frame.size.height - width, myView.frame.size.width, width)
-            border.frame = CGRectMake(-4, myView.frame.size.height + 2 - linewidth, width - 30, linewidth)
+            //border.frame = CGRectMake(5, 10, width, 10)
+            border.frame = CGRectMake(-5, myView.frame.size.height + 3 - linewidth, width - 30, linewidth)
             myView.layer.addSublayer(border)
         }
-        addBottomBorder(UIColor.lightGrayColor(), linewidth: 0.5, myView: patientNameReimburse)
-        addBottomBorder(UIColor.darkGrayColor(), linewidth: 0.5, myView: claimNo)
-        addBottomBorder(UIColor.blackColor(), linewidth: 0.5, myView: requestedAmountReimburseOne)
+        func addBottomBorderView(color: UIColor, linewidth: CGFloat, myView:UIView) {
+            let border = CALayer()
+            border.backgroundColor = color.CGColor
+            //border.frame = CGRectMake(5, 10, width, 10)
+            border.frame = CGRectMake(-3, myView.frame.size.height + 2 - linewidth, 250, 0.5)
+            myView.layer.addSublayer(border)
+        }
+        
+        addBottomBorder(UIColor.blackColor(), linewidth: 0.5, myView: patientNameReimburseTwo)
+        addBottomBorder(UIColor.lightGrayColor(), linewidth: 0.5, myView: claimNoReimburseTwo)
+        addBottomBorder(UIColor.darkGrayColor(), linewidth: 0.5, myView: requestedAmountReimburseTwo)
+        addBottomBorderView(UIColor.lightGrayColor(), linewidth: 0.5, myView: claimSupport)
+        // Do any additional setup after loading the view.
+        
+        //dashedline
+        let line = drawDottedLine(frame: CGRectMake(60, 175, 1.5, 275))
+        line.backgroundColor = UIColor.clearColor()
+        requestReceivedImage.layer.zPosition = 10
+        settledImage.layer.zPosition = 10
+        pendingImage.layer.zPosition = 10
+        reimburseView.addSubview(line)
+        claimSupport.layer.zPosition = 10
+        self.number1.layer.cornerRadius = 8
+        self.number1.layer.borderColor = UIColor.grayColor().CGColor
+        self.popupView.layer.zPosition = 40
+        self.number2.layer.cornerRadius = 8
         if myClaim == 0 {
             if activeClaim == 0 {
                 
@@ -156,7 +164,7 @@ class ReimbursementOneController: UIViewController,UIScrollViewDelegate {
                             self.closedClaimsJSON = self.claimsJSON["result"]["closedClaims"]
                             dispatch_async(dispatch_get_main_queue(),{
                                 print("ALL IS WELL");
-                               
+                                
                                 
                                 
                                 if(self.closedClaimsJSON!.count == 0 ) {
@@ -227,7 +235,7 @@ class ReimbursementOneController: UIViewController,UIScrollViewDelegate {
                             self.claimsJSON = json
                             self.closedClaimsJSON = self.claimsJSON["result"]["closedClaims"]
                             dispatch_async(dispatch_get_main_queue(),{
-                               
+                                
                                 
                                 
                                 if(self.closedClaimsJSON!.count == 0 ) {
@@ -251,22 +259,59 @@ class ReimbursementOneController: UIViewController,UIScrollViewDelegate {
             }
         }
         
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "KnowMore" {
-            let controller1 = segue.destinationViewController as! newDemoViewController
-            if activeClaim == 0{
+    
+    @IBAction func showPopup(sender: UIButton) {
+        UIView.animateWithDuration(0.3, animations: {
+            self.view.layoutIfNeeded()
+            self.popupView.alpha = 1
+            self.popupView.layer.zPosition = 40
+            self.reimburseView.alpha = 0.5
+        })
+        
+    }
+    
+    
+    @IBAction func closePopup(sender: UIButton)
+    {
+        UIView.animateWithDuration(0.3, animations: {
+            self.view.layoutIfNeeded()
             
-           controller1.nameReimburseTwo = claimsJSON["result"]["PendingClaims"][0]["PatientName"].string!
-                print("\(claimsJSON["result"]["PendingClaims"][0]["PatientName"].string!)")
-            
-            }
+            self.popupView.alpha = 0
+            self.reimburseView.alpha = 1
+        })
+        
+    }
+    var terms = false
+    @IBAction func call1(sender: AnyObject) {
+        
+        terms = !terms
+        if terms {
+            //self.termsCheckbox.backgroundColor = UIColor.orangeColor()
+            self.number1.setImage(UIImage(named: "checkbox_tick"), forState: .Normal)
+            print(true)
+        } else{
+            //self.termsCheckbox.backgroundColor = nil\
+            self.number1.setImage(UIImage(named: "checkbox_untick"), forState: .Normal)
+            print("false")
         }
     }
-    @IBAction func knowMoreButton(sender: UIButton) {
-        //nothing
+    var terms1 = false
+    @IBAction func call2(sender: AnyObject) {   terms1 = !terms1
+        if terms {
+            //self.termsCheckbox.backgroundColor = UIColor.orangeColor()
+            self.number2.setImage(UIImage(named: "checkbox_tick"), forState: .Normal)
+            print("true1")
+        } else{
+            //self.termsCheckbox.backgroundColor = nil\
+            self.number2.setImage(UIImage(named: "checkbox_untick"), forState: .Normal)
+            print("false1")
+        }
+        
         
     }
     override func didReceiveMemoryWarning() {
@@ -284,5 +329,18 @@ class ReimbursementOneController: UIViewController,UIScrollViewDelegate {
      // Pass the selected object to the new view controller.
      }
      */
+    
+}
+class drawDottedLine2: UIView {
+    override func drawRect(rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext() //Initializing the line
+        CGContextSetLineWidth(context!, 5.0) //Set the points of the line
+        CGContextSetStrokeColorWithColor(context!, mainBlueColor.CGColor) //set colour
+        CGContextSetLineDash(context!, 0, [5.5], 1) // if equal lengths: [7.5 pts colored, 7.5 pts empty] else: [1,2] //Set Line dash
+        CGContextSetLineCap(context!, CGLineCap(rawValue: 500)!) //line border radius
+        CGContextMoveToPoint(context!, 0, 0) //initial point and end point on the x an y axes
+        CGContextAddLineToPoint(context!, 0, 1000)
+        CGContextStrokePath(context!) //To actually make the line
+    }
     
 }
