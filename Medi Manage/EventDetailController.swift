323f40ce1ebfebec6908a8d603347e60abd3e9ad
Harsh Thakkar <harshthakkar46@gmail.com>
2017-01-19 19:08:30 +0530

    changes

--- Medi Manage/EventDetailController.swift
+++ Medi Manage/EventDetailController.swift
@@ -28,9 +28,13 @@ class EventDetailController: UIViewController {
         verticalLayout.backgroundColor = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)
         scrollView.addSubview(verticalLayout)
         
+//        verticalLayout.addSubview(subHeaderView())
         verticalLayout.addSubview(eventMapView())
         verticalLayout.addSubview(eventDetailView())
         verticalLayout.addSubview(eventRegistrationView())
+        verticalLayout.addSubview(eventDescriptionView())
+        verticalLayout.addSubview(registerButton())
+        verticalLayout.addSubview(spaceView())
         
         addHeightToLayout()
     }
@@ -62,9 +66,40 @@ class EventDetailController: UIViewController {
     }
     
     func eventRegistrationView() -> UIView {
-        let registratioView = eventRegistration(frame: CGRect(x: 10, y: 10, width: self.view.frame.size.width - 20, height: 75))
+        let registratioView = eventRegistration(frame: CGRect(x: 10, y: 20, width: self.view.frame.size.width - 20, height: 75))
         addShadow(registratioView)
         return registratioView
+    }
+    
+    func eventDescriptionView() -> UIView {
+        let descriptionView = eventDescription(frame: CGRect(x: 10, y: 20, width: self.view.frame.size.width - 20, height: 45))
+        addShadow(descriptionView)
+        return descriptionView
+    }
+    
+    func registerButton() -> UIView {
+        let registerButtonView = UIView(frame: CGRect(x: self.view.frame.width - 110, y: 20, width: 100, height: 40))
+        registerButtonView.backgroundColor = UIColor(red: 244 / 255, green: 109 / 255, blue: 30 / 255, alpha: 1)
+        registerButtonView.layer.cornerRadius = 5.0
+        registerButtonView.clipsToBounds = true
+        
+        let registerLabel = UILabel(frame: CGRect(x: 10, y: 0, width: registerButtonView.frame.width - 20, height: registerButtonView.frame.height))
+        registerLabel.text = "REGISTER"
+        registerLabel.font = UIFont(name: "Aviner Roman", size: 16)
+        registerLabel.textColor = UIColor.whiteColor()
+        registerButtonView.addSubview(registerLabel)
+        return registerButtonView
+    }
+    
+    func spaceView() -> UIView {
+        let spaceView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 20))
+        spaceView.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0)
+        return spaceView
+    }
+    
+    func subHeaderView() -> UIView {
+        let headerView = subHeader(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 60))
+        return headerView
     }
     
     func addShadow(view: UIView) {
---
+++ Medi Manage/eventDescription.swift
@@ -0,0 +1,32 @@
+//
+//  eventDescription.swift
+//  mpowerplus
+//
+//  Created by Wohlig Technology on 1/19/17.
+//  Copyright © 2017 Wohlig Technology. All rights reserved.
+//
+
+import UIKit
+
+class eventDescription: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib ()
+    }
+    
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "eventDescription", bundle: bundle)
+        let eventDescription = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        eventDescription.frame = bounds
+        eventDescription.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(eventDescription)
+    }
+    
+}
